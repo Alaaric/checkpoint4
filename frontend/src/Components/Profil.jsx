4282ac3de24fff831769fb07f0038a525d727714
Alaric HENROT <alaric.henrot@gmail.com>
2023-07-19 19:07:16 +0200

    modif profil basic inputs

--- frontend/src/Components/Profil.jsx
+++ frontend/src/Components/Profil.jsx
@@ -1,3 +1,32 @@
+import { useContext } from "react";
+import userContext from "../contexts/userContext";
+
 export default function Profil() {
-  return <div className="profil"></div>;
+  const { user, setUser } = useContext(userContext);
+
+  return (
+    <div className="profil">
+      <h2>modifier mes information</h2>
+      <form>
+        <label htmlFor="">
+          nom:
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          prénom:
+          <input type="text" />
+        </label>
+        <label htmlFor="">
+          email :
+          <input type="email" />
+        </label>
+        mot de passe:
+        <label htmlFor="">
+          <input type="password" />
+        </label>
+      </form>
+      <button type="submit">appliquer les changements</button>
+      <button type="button">supprimer mon compte</button>
+    </div>
+  );
 }
