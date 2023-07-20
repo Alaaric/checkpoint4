942a5ec22cdf75ac3bac5f5b1263b6a8fad88c96
Alaric HENROT <alaric.henrot@gmail.com>
2023-07-20 10:56:47 +0200

    front routes protection done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./scss/styles.scss";
-import NavBar from "./Components/NavBar";
-import Footer from "./Components/Footer";
 import AllAds from "./Pages/AllAds";
 import PostAd from "./Pages/PostAd";
 import Home from "./Pages/Home";
 import MyAccount from "./Pages/MyAccount";
+import NavLayout from "./Layouts/NavLayout";
+import ProtectedLayout from "./Layouts/ProtectedLayout";
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ads" element={<AllAds />} />
-        <Route path="/postads" element={<PostAd />} />
-        <Route path="/account" element={<MyAccount />} />
+        {/* public routes */}
+        <Route element={<NavLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/ads" element={<AllAds />} />
+
+          {/* private routes  */}
+          <Route element={<ProtectedLayout />}>
+            <Route path="/postads" element={<PostAd />} />
+            <Route path="/account" element={<MyAccount />} />
+          </Route>
+        </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }
